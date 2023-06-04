a1fc32d8b9c5bbf5b9c4645dca8cb7f2b0bb0b79
Manish Squadkin <support@squadkin.com>
2023-06-04 15:33:21 +0530

    blog 1 updated

--- pages/blogs/blog1.js
+++ pages/blogs/blog1.js
@@ -1,28 +1,9 @@
 import React from 'react'
-import { useRouter } from 'next/router'
 
-// export const getStaticPath = async () => 
-
-export const getStaticProps = async () => {
-  // const router = useRouter();
-  // const blogId = router.query.blogno;
-  // const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${blogId}`);
-  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/2`);
-  const data = await res.json();
-  return { props: { data } };
-}
-
-const myData = ({data}) => {
+const blog1 = () => {
   return (
-    <>
-      {data.map((item) => {
-        return <div key={item.id}>
-            <h1>{item.title}</h1>
-            <p>{item.body}</p>
-        </div>
-      })}
-    </>
+    <div>blog1</div>
   )
 }
 
-export default myData
+export default blog1
